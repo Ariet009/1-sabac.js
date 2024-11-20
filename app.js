4bd773f16d09cca973cc501020f70c19d4566a1e
Ariet009 <arietjumabekov01@gmail.com>
2024-11-20 06:42:33 +0300

    create a new project

---
+++ app.js
@@ -0,0 +1,35 @@
+// переменный let var coluvnRuleStyle  
+// var ESS  
+// let const ES6 (EcmaScript6) 
+ 
+//     const myName = 'Aman'; 
+//     let myAge = 12; 
+ 
+     
+ 
+    // let message = 'Hello ' + myName + '  ' + myAge + 'Wellcome to JS'; //ESS 
+    // let newMessage = Hello ${myName} ${myAge} Wellcome to JS; // ES6 
+    // console.log(message); 
+    // console.log(newMessage); 
+ 
+    const userName=prompt('What is your name')  
+console.log(userName);  
+  
+alert=('userName')  
+ 
+const It=confirm('Are you an IT specialist?')    
+      console.log(It)   
+ 
+const whereDoYouLive=prompt('where Do You Live') 
+      console.log(whereDoYouLive) 
+ 
+// const num = parseInt(prompt("first san")) 
+// const num2 =parseInt(prompt('Second number')) 
+// const summa = num + num2 
+// console.log(summa); 
+// const minus=num-num2 
+// console.log(num+'-'+num2+'='+minus);  
+// const umnoj=num*num2 
+// console.log(umnoj); 
+// const deleniye = num / num2 
+// console.log(deleniye);
